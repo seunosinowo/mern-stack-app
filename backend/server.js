0d5a7d2e211265abd5d48d7ac4b78432f86d6967
seunosinowo <oluwaseunpaul98@gmail.com>
2025-07-05 11:54:35 +0100

    Get, Delete and Post endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,55 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js'
 dotenv.config()
 
 const app = express();
 
-app.get('products', (req, res) => {})
+//middle ware to accept json data
+app.use(express.json()); //allow us to accept json data in req.body
 
-// console.log(process.env.MONGO_URL)
-
-app.get("/", (req, res) => {
-    res.send("Server is ready11")
+//Test Get Endpoints
+app.get("/api/products", async (req, res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products})
+    }catch(error){
+        res.status(500).json({success: false, message: "Server Error"})
+        console.log("error in fetching products:", error.message)
+    }
 })
+
+//Set a post endpoint
+app.post("/api/products", async (req, res) => {
+    const product = req.body; //user will send this data
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product)
+    try{
+        await newProduct.save()
+        res.status(201).json({success:true, data:newProduct})
+    }catch(error){
+        console.log("Error in creating product", error.message)
+        res.status(500).json({success: false, message: "Server Error"})
+    }
+})
+
+//Setting up Delete Endpoints
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true, message: "Product deleted"}) 
+    }catch(error){
+        res.status(404).json({success:false, message: "Product not found"})
+        console.log("error in deleting product: ", error.message)
+    }
+})
+
 
 app.listen(5000, () => {
     connectDB();
